f1deef80e0007764c8f103289fbce19d33b89aec
210016299 <210016299@CPT-EN-113D08.ADS.CPUT.AC.ZA>
2014-05-06 18:52:04 +0200

    Changed title HollywoodHotelBetting

---
+++ HollywoodHotelBetting/src/main/java/com/joseph/california/services/TotalPeopleService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.joseph.california.services;
+
+import com.joseph.california.domain.Person;
+import java.util.List;
+
+/**
+ *
+ * @author BradleyJoseph
+ */
+public interface TotalPeopleService {
+    public List<Person> getTotalPeople();
+    
+}
